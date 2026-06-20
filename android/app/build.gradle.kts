1a4b8465bf4b671b16c58f20a986f9c8fb6e2812
Ziad <ziadtamer.dev1@gmail.com>
2026-06-20 11:01:51 -0700

    تصحيح اصدار المكتبات

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.zekra1"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 34
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
